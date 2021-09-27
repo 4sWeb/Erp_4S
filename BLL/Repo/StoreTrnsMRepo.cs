f99f3a416fc2d8bd7797e2d95cea1a9a89883775
4stechgit <4STechgit@gmail.com>
2021-09-27 09:25:45 +0200

    enhancement-AllTransByDepID-

--- BLL/Repo/StoreTrnsMRepo.cs
+++ BLL/Repo/StoreTrnsMRepo.cs
@@ -72,8 +72,8 @@ namespace BLL.Repo
                 {
                     Dictionary<string, object> Current = (Dictionary<string, object>)item;
                     var NewItem = GetByCondition(c => c.StoreTrnsMId == decimal.Parse(Current.GetValueOrDefault("MID").ToString())).Result.Select(s => new { s.TrnsCode, s.TrnsDate, s.TrnsNo, s.StoreTrnsMId, s.FromStoreAllcodesId, s.ToStoreAllcodesId, s.BranchId, s.Storedocnum, s.Period }).FirstOrDefault();
-                 
-                    
+                    var Exist = Items.Where(c => c.StoreTrnsMId == NewItem.StoreTrnsMId);
+                    if(Exist.Count()==0)
                     Items.Add(new Transaction_VM
                     {
                         StoreTrnsMId = NewItem.StoreTrnsMId,
